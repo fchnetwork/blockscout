e642c7abc142ffc45c104f8fc3c0d8f890f32798
Victor Baranov <baranov.viktor.27@gmail.com>
2019-11-25 17:26:52 +0300

    Update migration: set unique index: block_hash, block_index

--- apps/explorer/priv/repo/migrations/20191103112058_create_internal_transactions_index_block_hash_block_index.exs
+++ apps/explorer/priv/repo/migrations/20191103112058_create_internal_transactions_index_block_hash_block_index.exs
@@ -2,6 +2,6 @@ defmodule Explorer.Repo.Migrations.CreateInternalTransactionsIndexBlockHashBlock
   use Ecto.Migration
 
   def change do
-	create(index(:internal_transactions, [:block_hash, :block_index]))
+	create(unique_index(:internal_transactions, [:block_hash, :block_index]))
   end
 end
